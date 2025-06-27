7b2c474781bc0b19f6b9880677c13c3fe74ef80d
subhamdagar73 <152770741+subhamdagar73@users.noreply.github.com>
2025-06-27 08:03:43 +0530

    Updated API_BASE_URL  api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
-//const API_BASE_URL = 'http://localhost:3001/api';
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001/api';
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://company-grow-deploy.onrender.com/api';
 
 export class ApiClient {
   private token: string | null = null;
